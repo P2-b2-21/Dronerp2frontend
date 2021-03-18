023d4c2141677adb4efbc581e06e968b1bbfd69f
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2021-03-18 15:30:09 +0100

    form work

--- form/form.js
+++ form/form.js
@@ -5,6 +5,7 @@ let choices = {
     gcd04: "",
 };
 
+/*
 function calculateGCD(obj) {
     //console.log(`${event.target.type}`);
     console.log("GCD status:");
@@ -13,46 +14,41 @@ function calculateGCD(obj) {
     }
     console.log("Final GCD: calculating..");
 }
+*/
 
 function addFormElement() {
     const mainForm = document.querySelector('#GRC');
     const newForm = document.createElement('form');
     newForm.id = "question01";
-    
     mainForm.appendChild(newForm);
 
     const form = document.querySelector('#question01');
-    let newElement = document.createElement("p");
-    newElement.id="q";
-    newElement.textContent = "VLOS-ELOS-BLOS?";
-
+    const newElement = document.createElement("fieldset");
+    newElement.id = "q";
+    newElement.innerHTML = '<legend> VLOS-ELOS-BLOS? </legend>'
     form.appendChild(newElement);
-    addLineBreak("q");
 
-    makeButton("q", "radio", "vlos", "VLOS-ELOS-BLOS", "a", "VLOS", undefined);
-    addLineBreak("label-vlos");
-    makeButton("q", "radio", "elos", "VLOS-ELOS-BLOS", "b", "ELOS", undefined);
-    addLineBreak("label-elos");
-    makeButton("q", "radio", "blos", "VLOS-ELOS-BLOS", "c", "BLOS", undefined);
-    addLineBreak("label-blos");
-    makeButton("q","button", undefined, undefined, "Submit", undefined, "test()"); // func not working
+    makeButton("q", "radio", "vlos", "VLOS-ELOS-BLOS", "a", "VLOS");
+    addLineBreak("q");
+    makeButton("q", "radio", "elos", "VLOS-ELOS-BLOS", "b", "ELOS");
+    addLineBreak("q");
+    makeButton("q", "radio", "blos", "VLOS-ELOS-BLOS", "c", "BLOS");
+    addLineBreak("q");
+    makeButton("q","button", undefined, undefined, "Submit", undefined);
 }
 
-function makeButton(idElement, buttonType, id, name, value, textContent, func) {
+function makeButton(idElement, buttonType, id, name, value, textContent) {
     const element = document.querySelector(`#${idElement}`);
+
     button = document.createElement("input");
     button.type = buttonType;
     button.id = id;
     button.name = name;
     button.value = value;
-    button.onclick = function(){`${func}`}; // func not working
-
     element.appendChild(button);
 
     label = document.createElement("label");
-    label.id = "label-" + id;
     label.textContent = textContent;
-
     element.appendChild(label);
 }
 
@@ -62,6 +58,20 @@ function addLineBreak(id) {
     return element.appendChild(lineBreak);
 }
 
-function test() {
-    console.log("Test");
-}
+// Not working
+document.querySelector("#GRC").addEventListener('change', (event) => {
+
+    choices.gcd01 = event.target.value;
+    console.log("->" + choices.gcd01);
+    console.log(event.target.parentElement.id);
+
+    if (event.target.parentElement.id == 'q') {
+
+        document.querySelector("#q").addEventListener('change', (event) => {
+            choices.gcd02 = event.target.value;
+            console.log("->" + choices.gcd02);
+            console.log("Do next form!");
+        });
+        console.log(choices.gcd01 + ":" + choices.gcd02);
+    };
+});
