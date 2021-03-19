8f49f283742d0923b981c984fd656a3491485a93
Jaffenheimer <74659222+Jaffenheimer@users.noreply.github.com>
2021-03-19 11:33:16 +0100

    Sidenav virker nu korrekt

--- fakta.js
+++ fakta.js
@@ -27,10 +27,10 @@ function make_paragraph(element = "", text = "") {
     document.getElementById("paragraph_subject").remove();
   }
   let type = document.createElement(element);
-
   let text_in_box = document.createTextNode(text);
 
   type.appendChild(text_in_box);
+
   type.id = "paragraph_subject";
 
   document.getElementById("text_box").appendChild(type);
@@ -52,9 +52,24 @@ make_button("Button", "Den specifikke kategori", "1", "fakta_topnav");
 make_button("Button", "Om - SORA", "2", "fakta_topnav");
 make_button("Button", "Hvem er vi", "3", "fakta_topnav");
 
+let j = [
+  "SORA - Trin 1",
+  "SORA - Trin 2",
+  "SORA - Trin 3",
+  "SORA - Trin 4",
+  "SORA - Trin 5",
+  "SORA - Trin 6",
+  "SORA - Trin 7",
+  "SORA - Trin 8",
+  "SORA - Trin 9",
+  "SORA - Trin 10",
+];
+
 const chosen_input = document.querySelector("#fakta_topnav");
+const chosen_input_2 = document.querySelector("#fakta_sidenav");
 //choose the correct type of text to display depending on the selected button
 function choose_correct_text() {
+  //remove buttons once another button is pressed
   if (document.getElementById("4") !== null) {
     for (let i = 4; i <= 13; i++) {
       document.getElementById(i).remove();
@@ -63,11 +78,14 @@ function choose_correct_text() {
   if (event.target.id === "1") {
     make_paragraph(
       "p",
-      "Indenfor den specifikke kategori, angående emnet droneflyvning, findes der yderligere tre ansøgningsmuligheder. Den første mulighed er at arbejde efter et såkaldt standardscenarie (STS). Et standardscenarie er nogle præ-definerede retningslinjer der gælder, så længe der bliver fløjet med droner under forudsætning af at disse retningslinjer overholdes. Ud over (STS) kan der laves to andre former for ansøgninger. Den ene kaldes Predefined Risk Assessment (PDRA) og den anden Specific Operations Risk Assessment (SORA). PDRA minder rigtig meget om førnævnte STS, da dette er prædefinerede retningslinjer der skal overholdes, og der derfor ikke eksisterer nogen fleksibilitet. Dog skal der dokumenteres mere omkring flyvningen fra operatørens side, end ved en STS.Her kommer SORA ind i billedet, da denne form for ansøgning giver operatøren mulighed for at skræddersy sin ansøgning til lige præcis den droneflyvning vedkommende har planlagt.Dette giver en masse fleksibilitet i ansøgningsprocessen og gør det derved muligt at tilpasse den fuldstændigt efter behov. Med dette kommer dog også en række besværligheder, da man bliver nødt til at sætte sig ind i væsentligt flere love, og retningslinjer, selv i henhold til den plan man har for sin flyvning. Derudover, er der en række sikkerhedsmæssige vurderinger, man igen selv skal tage stilling til, eller i hvertfald dokumenterer hvad man vil foretage sig i tilfælde af en nødsituation. Dette gør det mindre overskueligt for firmaer, der har brug for at flyve med en drone et pågældende sted hvor denne nye SORA-proces bliver implementeret som i Danmark. Dette giver en masse fleksibilitet i ansøgningsprocessen og gør det derved muligt at tilpasse den fuldstændigt efter behov. Med dette kommer dog også en række besværligheder, da man bliver nødt til at sætte sig ind i væsentligt flere love, og retningslinjer, selv i henhold til den plan man har for sin flyvning. Derudover, er der en række sikkerhedsmæssige vurderinger, man igen selv skal tage stilling til, eller i hvertfald dokumenterer hvad man vil foretage sig i tilfælde af en nødsituation. Dette gør det mindre overskueligt for firmaer, der har brug for at flyve med en drone et pågældende sted hvor denne nye SORA-proces bliver implementeret som i Danmark."
+      "Indenfor den specifikke kategori, angående emnet droneflyvning, findes der yderligere tre ansøgningsmuligheder. Den første mulighed er at arbejde efter et såkaldt standardscenarie (STS). Et standardscenarie er nogle præ-definerede retningslinjer der gælder, så længe der bliver fløjet med droner under forudsætning af at disse retningslinjer overholdes. Ud over (STS) kan der laves to andre former for ansøgninger. Den ene kaldes Predefined Risk Assessment (PDRA) og den anden Specific Operations Risk Assessment (SORA). PDRA minder rigtig meget om førnævnte STS, da dette er prædefinerede retningslinjer der skal overholdes, og der derfor ikke eksisterer nogen fleksibilitet. Dog skal der dokumenteres mere omkring flyvningen fra operatørens side, end ved en STS. Her kommer SORA ind i billedet, da denne form for ansøgning giver operatøren mulighed for at skræddersy sin ansøgning til lige præcis den droneflyvning vedkommende har planlagt.Dette giver en masse fleksibilitet i ansøgningsprocessen og gør det derved muligt at tilpasse den fuldstændigt efter behov. Med dette kommer dog også en række besværligheder, da man bliver nødt til at sætte sig ind i væsentligt flere love, og retningslinjer, selv i henhold til den plan man har for sin flyvning. Derudover, er der en række sikkerhedsmæssige vurderinger, man igen selv skal tage stilling til, eller i hvertfald dokumenterer hvad man vil foretage sig i tilfælde af en nødsituation. Dette gør det mindre overskueligt for firmaer, der har brug for at flyve med en drone et pågældende sted hvor denne nye SORA-proces bliver implementeret som i Danmark. Dette giver en masse fleksibilitet i ansøgningsprocessen og gør det derved muligt at tilpasse den fuldstændigt efter behov. Med dette kommer dog også en række besværligheder, da man bliver nødt til at sætte sig ind i væsentligt flere love, og retningslinjer, selv i henhold til den plan man har for sin flyvning. Derudover, er der en række sikkerhedsmæssige vurderinger, man igen selv skal tage stilling til, eller i hvertfald dokumenterer hvad man vil foretage sig i tilfælde af en nødsituation. Dette gør det mindre overskueligt for firmaer, der har brug for at flyve med en drone et pågældende sted hvor denne nye SORA-proces bliver implementeret som i Danmark."
     );
   } else if (event.target.id === "2") {
+    //Make buttons to the different sora steps
+    let p = 0;
     for (let i = 4; i <= 13; i++) {
-      make_button("Button", "SORA - Trin 1", i, "fakta_sidenav");
+      make_button("Button", j[p], i, "fakta_sidenav");
+      p++;
     }
     make_paragraph(
       "p",
@@ -80,4 +98,30 @@ function choose_correct_text() {
     );
   }
 }
+//Text function for sidebar
+function choose_correct_text_2() {
+  if (event.target.id === "4") {
+    make_paragraph("p", "hej");
+  } else if (event.target.id === "5") {
+    make_paragraph("p", "med");
+  } else if (event.target.id === "6") {
+    make_paragraph("p", "dig");
+  } else if (event.target.id === "7") {
+    make_paragraph("p", "hej");
+  } else if (event.target.id === "8") {
+    make_paragraph("p", "med");
+  } else if (event.target.id === "9") {
+    make_paragraph("p", "dig");
+  } else if (event.target.id === "10") {
+    make_paragraph("p", "hej");
+  } else if (event.target.id === "11") {
+    make_paragraph("p", "med");
+  } else if (event.target.id === "12") {
+    make_paragraph("p", "dig");
+  } else if (event.target.id === "13") {
+    make_paragraph("p", "hej");
+  }
+}
+
 chosen_input.addEventListener("click", choose_correct_text);
+chosen_input_2.addEventListener("click", choose_correct_text_2);
