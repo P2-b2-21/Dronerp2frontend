1ad419c3b98c84541287799ca198997d74dbd81c
Andreas Knudsen Alstrup <andreas090994@gmail.com>
2021-03-24 17:10:26 +0100

    initPage()

--- form/form.js
+++ form/form.js
@@ -7,6 +7,7 @@ let gcdInput = {
 
 const questionContent = {
     question02: {
+        userInput: "",
         ask: "Er jeg spørgsmål 2?",
         choices: {
             choice0: {
@@ -24,6 +25,7 @@ const questionContent = {
         },
     },
     question03: {
+        userInput: "",
         ask: "Er jeg spørgsmål 3?",
         choices: {
             choice0: {
@@ -38,180 +40,142 @@ const questionContent = {
                 text: "XD",
                 value: "CC",
             },
+            choice3: {
+                text: "8D",
+                value: "DD",
+            },
+        },
+    },
+    question04: {
+        userInput: "",
+        ask: "Er jeg spørgsmål 4?",
+        choices: {
+            choice0: {
+                text: "A",
+                value: "AAA",
+            },
+            choice1: {
+                text: "B",
+                value: "BBB",
+            },
+            choice2: {
+                text: "C",
+                value: "CCC",
+            },
         },
     },
 };
 
-//console.log(Object.entries(questionContent));
-
-/*let name = [
-    "question01",
-    "question02",
-    "question03"
-];
-*/
-
-//console.log(name[1]);
-
-/*
-let p = new Promise((resolve, reject) => {
-    
+window.addEventListener('load', () => {
+    console.log("Page is loaded");
+    //initPage();
+    makePage("form01", 0, 1);
 });
 
-nextQuestionPromise().then(() => {
-    
-})
 
-let nextQuestion = function fireOnce(callback) {
-    if (nextQuestion.fired) return;
-    nextQuestion.fired = true;
-    callback();
-    console.log("q1 only onece");
+function initPage() {
+    let ques = Object.keys(questionContent)[0];
+    let numOfButtons = Object.keys(questionContent[`${ques}`].choices).length;
+
+    addFormElement(`form0${0}`, `${ques}`, questionContent[`${ques}`].ask, "radio", numOfButtons);
+
 };
-*/
 
-// Create question and log user input
 
-/*
-document.querySelector('#form01').addEventListener('change', addFormOnce('form01'))
-    .then( () => {
-        console.log("Need to wait");
-        document.querySelector('#form02').addEventListener('change', addFormOnce('form02'))
-    });
-*/
 
-//document.querySelector('#form01').addEventListener('change', formCaller());
-//formCaller();
-
-//let question02 = document.querySelector("#GRC").childNodes;
-
-//console.log(question02.childNodes);
-//console.log(question02.childNodes.nextElementSibling);
-
-/*
-let ques = Object.keys(questionContent)[0];
-
-console.log(`${Object.keys(questionContent)[0]}`);
-console.log(questionContent[`${ques}`].ask);
-console.log(`${ques}`);
-console.log(Object.keys(questionContent).length);
-*/
-
-//let i = 0;
-//let k = 2;
-//let flag = false;
-function makePage(form, i, k) {
-    let ques = Object.keys(questionContent)[i];
-
-    console.log(i + ":" + Object.keys(questionContent)[i]);
-    console.log(`form0${k}`, `${ques}`, questionContent[`${ques}`].ask, "radio", 3);
-
-    document.querySelector(`#${form}`).addEventListener('change', () => {
-        addFormElement(`form0${k}`, `${ques}`, questionContent[`${ques}`].ask, "radio", 3);
-    }, {once: true});
-
-    i++;
-    k++;
-
-    console.log(Object.keys(questionContent).length + ":" + i);
-
-    
-    if (Object.keys(questionContent).length == i) return;
-
-    makePage(`form0${i}`, i, k);
-}
-
-makePage("form01", 0, 2, true);
-
-/* WOKING
-document.querySelector(`#form01`).addEventListener('change', () => {
-    addFormElement("form02", "question02", "Er jeg spørgsmål 2?", "radio", 3);
-
-    document.querySelector(`#form02`).addEventListener('change', () => {
-        addFormElement("form03", "question03", "Er jeg spørgsmål 3?", "radio", 3);
-    }, {once: true});
-
-}, {once: true});
-*/
-
-/*
-document.querySelector(`#form02`).addEventListener('change', () => {
-    addFormElement("form03", "question03", "Er jeg spørgsmål 3?", "radio", 3);
-}, {once: true});
-*/
-
-//document.querySelector(`#form01`).removeEventListener('change', handler);
-
-/*
-document.querySelector(`#form01`).removeEventListener('change', function(e) {
-    addFormElement("form02", "question02", "Er jeg spørgsmål 2?", "radio", 3);
-    document.querySelector(`#form03`).addEventListener('change', console.log("add 03"));
-    //form = "asd";
-});
-*/
-
-//document.querySelector('#form02').addEventListener('change', changeOnce02);
-
-//asyncFN();
+/* Create questions and log user input */
+//makePage("form01", 0, 1);
 
 const question = document.querySelector("#GRC");
 question.addEventListener('change', (event) => {
     
+    //let objLen = Object.keys(questionContent).length;
+    //let ques = Object.keys(questionContent)[i];
+    
+    let ques;
+    switch (event.target.name) {
+        case "question01": {
+            ques = "question01";
+        };
+        break;
+        case "question02": {
+            ques = "question02"
+        };
+        break;
+        case "question03": {
+            ques = "question03"
+        };
+        break;
+        case "question04": {
+            ques = "question04"
+        break;
+        };
+    };
+    questionContent[`${ques}`].userInput = event.target.value;
+
+
+    /*
     if(event.target.name == "question01") {
         gcdInput.input01 = event.target.value;
-
-        /*
-        nextQuestion((function() {
-            addFormElement("form02", "question02", "Er jeg spørgsmål 2?", "radio", 3);
-        }));
-        */
+        questionContent["question02"].userInput = event.target.value;
+        console.log("USERINPUT: " + questionContent["question02"].userInput);
     };
     
     if (event.target.name == "question02") {
         gcdInput.input02 = event.target.value;
-        //document.querySelector('#question02').addEventListener('change', addFormOnce);
-
-        //addFormElement("form03", "question03", "Er jeg spørgsmål 3?", "radio", 3);
-
-        //if (event.target.name != "question02") return;
-
-        /*
-        nextQuestion((function() {
-            addFormElement("form03", "question03", "Er jeg spørgsmål 3?", "radio", 3);
-        }));
-        */
     };
-
+    
     if (event.target.name == "question03") {
         gcdInput.input03 = event.target.value;
     };
+    if (event.target.name == "question04") {
+        gcdInput.input04 = event.target.value;
+    };
+    */
     
     // Working test
-    console.log(gcdInput.input01 + "->" + gcdInput.input02 + "->" + gcdInput.input03);
+    /*
+    console.log(gcdInput.input01 + "->" + 
+    gcdInput.input02 + "->" + 
+    gcdInput.input03 + "->" +
+    gcdInput.input04);
+    */
+    console.log(">>>>"+questionContent["question02"].userInput + "->" + 
+    questionContent["question03"].userInput + "->" + 
+    questionContent["question04"].userInput + "->");
+
 });
 
-/*
-function addFormOnce(form) {
-    return new Promise((resolve) => {
-        console.log("Do stuff once");
-        document.querySelector(`#${form}`).addEventListener('change', (event) => {
-            addFormElement(`${form}`, "question02", "Er jeg spørgsmål 2?", "radio", 3);
-            document.querySelector(`#${form}`).removeEventListener('change', addFormOnce("form03"));
+/* -----page-functions-------------------------------------------------------------------------------------- */
+function makePage(form, i, k) {
+    console.log("input " + `gcdInput0${i+1}`);
+    if (Object.keys(questionContent).length == i) return;
+    let ques = Object.keys(questionContent)[i];
+    let numOfButtons = Object.keys(questionContent[`${ques}`].choices).length;
+
+    console.log(i + ":" + Object.keys(questionContent)[i]);
+    console.log(`form0${k}`, `${ques}`, questionContent[`${ques}`].ask, "radio", numOfButtons);
+
+    new Promise(function(resolve, reject) {
+        document.querySelector(`#${form}`).addEventListener('change', () => {
+            addFormElement(`form0${k}`, `${ques}`, questionContent[`${ques}`].ask, "radio", numOfButtons);
+            console.log("Before resolve()");
             resolve();
-        });
+            console.log("After resolve()");
+        }, {once: true});
+    })
+
+    .then(function() {
+        console.log("then:");
+        i++;
+        k++;
+        console.log(`makePage - i:${i} k:${k}`);
+        makePage(`form0${i}`, i, k);
+    }, function(err) {
+        console.log(err);
     });
 };
 
-async function formCaller() {
-    console.log("Caller");
-    await addFormOnce("form01");
-    console.log("After waiting");
-    //await addFormOnce("form03");
-}
-*/
-//document.querySelector('#question02').addEventListener('change', addFormOnce);
-
-/* -----page-functions-------------------------------------------------------------------------------------- */
 function addFormElement(formID, questionID, questionName, typeOfButton, numOfButtons) {
     const mainForm = document.querySelector('#GRC');
     const newForm = document.createElement('form');
