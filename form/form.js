b98d0531e94e55f8221f43ff9ecb1762a4c3a7a9
Malthe Morsing Larsen <malthe@morsingdata.dk>
2021-04-29 12:33:07 +0200

    Update form.js

--- form/form.js
+++ form/form.js
@@ -1,3 +1,10 @@
+let developmentMode = true;
+let frontendAddr = developmentMode ? "127.0.0.1:5500" : "server.malthelarsen.dk";
+let backendAddr = developmentMode ? "127.0.0.1:3000" : "server.malthelarsen.dk:3000";
+
+console.log(frontendAddr);
+console.log(backendAddr);
+
 /* **********************
    On page load
    initialising and making page
@@ -6,12 +13,12 @@
 */
 let questionContent = {};
 fetch(`questions_${MAINFORM}.json`)
-.then((response) => {return response.json()})
-.then((data) => {
-    questionContent = data;
-    initPage(0, 1);
-    makePage("form1", 1, 2);
-});
+    .then((response) => { return response.json() })
+    .then((data) => {
+        questionContent = data;
+        initPage(0, 1);
+        makePage("form1", 1, 2);
+    });
 
 const question = document.querySelector(`#${MAINFORM}`);
 
@@ -19,7 +26,7 @@ const question = document.querySelector(`#${MAINFORM}`);
    ********************** 
    GRC handling
    **********************
-*/ 
+*/
 
 if (MAINFORM === "GRC") {
     console.log(`questions_${MAINFORM}.json.. loaded`)
@@ -84,65 +91,77 @@ let mitigationMatrix = [
    ********************** 
    ARC handling
    **********************
-*/ 
+*/
+
+
 
 if (MAINFORM === "ARC") {
-    console.log(GRC)
+
+    let params = new URLSearchParams(location.search);
+    GRC = params.get('GRC');
+
+    if (GRC == undefined) {
+        alert("GRC not found, redirecting to GRC");
+        window.location.href = `http://${frontendAddr}/form/grc.html`;
+    }
+
+    console.log(GRC);
+
     console.log(`questions_${MAINFORM}.json.. loaded`)
-    
+
     makeSubmitButton(postFinalARC, "POSTING TO SERVER & GOES TO BRUGERPROFIL", "../brugerprofil.html");
 
     let userInput = [];
 
-    question.addEventListener('change', (event) => {    
+    question.addEventListener('change', (event) => {
         let currNode = event.target.parentNode.parentNode;
-        
+
         // Executes when ARC value has been selected
         if (event.target.value !== 'subquestion' && event.target.value !== 'next-question') {
-            
+
             // Make submit button available
             document.querySelector('[name="submitBtn"]').disabled = false;
-            
+
             // Hides all questions that comes after currently clicked question
             findAllNextElementSiblings(currNode).forEach((element) => {
                 element[0].style.display = "none";
-                
+
                 /* TO DO: catch error */
                 // Uncheck radio buttons
                 element[0].childNodes[1].checked = false;
                 element[0].childNodes[4].checked = false;
             });
-            
+
         } else {
             // Gray out toggle submit button
             document.querySelector('[name="submitBtn"]').disabled = true;
         }
-        
+
         if (event.target.value === 'next-question') {
             let nextID = currNode.nextSibling.childNodes[0].id
             document.querySelector(`#${nextID}`).style.display = "";
         }
-        
+
         if (currNode.id.replace(/[0-9]/, '') === 'form' && doesSubQuestionPathExist() === 1) {
             removeSubQuestionPath(currNode);
         }
-        
+
         // Makes next sub questiong path
         if (event.target.value === 'subquestion' && doesSubQuestionPathExist() === 0) {
             let questionOBJ = questionContent[`${event.target.parentNode.id}`];
             questionOBJ = ((((questionOBJ || {}).choices || {}).choice0 || {}).subquestion0 || {});
             initSubQuestionPath(questionOBJ, 0);
             initSubQuestionButtons();
-            
+
             let nextID = currNode.nextSibling.childNodes[0].id
             document.querySelector(`#${nextID}`).style.display = "";
         }
-        
+
         /* TO DO: use id: form4 */
         if (event.target.value === 'next-question' && doesSubQuestionPathExist() === 0) {
             document.querySelector('#form4').style.display = "";
         }
-        
+
         if (event.target.value === 'subquestion' && doesSubQuestionPathExist() === 1) {
             let nextID = currNode.nextSibling.childNodes[0].id
             document.querySelector(`#${nextID}`).style.display = "";
@@ -150,32 +169,35 @@ if (MAINFORM === "ARC") {
 
         // Log user input
         userInput += "," + event.target.value;
+        ARC = event.target.value;
         console.log(userInput);
     });
 };
-    
- /*
-   ********************** 
-   Page functions
-   **********************
-*/ 
-    
+
+let ARC;
+
+/*
+  ********************** 
+  Page functions
+  **********************
+*/
+
 /* Create questions from JSON Object */
 function makePage(form, quesNum, formNum) {
     if (Object.keys(questionContent).length == quesNum) return;
-    
+
     // TODO: do name change */
     let currentQuestion = Object.keys(questionContent)[quesNum];
     console.log("currentQuestion: " + currentQuestion);
-    
+
     let numOfButtons = Object.keys(questionContent[`${currentQuestion}`].choices).length;
-    
+
     document.querySelector(`#${form}`).addEventListener('change', (event) => {
         addFormElement(`form${formNum}`, `${currentQuestion}`, questionContent[`${currentQuestion}`].ask, "radio", numOfButtons);
         quesNum++;
         formNum++;
         makePage(`form${quesNum}`, quesNum, formNum);
-    }, {once: true});
+    }, { once: true });
 };
 
 function initSubQuestionPath(obj, i) {
@@ -199,57 +221,57 @@ function findAllNextElementSiblings(node) {
 };
 
 function initSubQuestionButtons() {
-    switch(event.target.parentNode.id) {
+    switch (event.target.parentNode.id) {
         case "question3": {
             event.target.parentNode.parentNode.nextSibling.style.display = "none";
             document.querySelector('#subquestion0').style.display = "";
-            
-        makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-d", "Ja");
-        addLineBreak(`subquestion${0}`);
-        makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-c", "Nej");
-    }
-    break;
-    case "question4": {
-        if (event.target.nextSibling.innerText === "Ja") {
+
             makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-d", "Ja");
             addLineBreak(`subquestion${0}`);
-            makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "ARC-d", "Ja");
-            addLineBreak(`subquestion${1}`);
-            makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "ARC-c", "Ja");
-            addLineBreak(`subquestion${2}`);
-            makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${3}`, 'radio', `subquestion${3}`, "ARC-c", "Ja");
-            addLineBreak(`subquestion${3}`);
-            
-        } else {
-            makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-c", "Ja");
-            addLineBreak(`subquestion${0}`);
-            makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "ARC-c", "Ja");
-            addLineBreak(`subquestion${1}`);
-            makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "ARC-c", "Ja");
-            addLineBreak(`subquestion${2}`);
-            makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "subquestion", "Nej");
-            
-            makeButton(`subquestion${3}`, 'radio', `subquestion${3}`, "ARC-b", "Ja");
-            addLineBreak(`subquestion${3}`);
+            makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-c", "Nej");
         }
-    }
-    break;
+            break;
+        case "question4": {
+            if (event.target.nextSibling.innerText === "Ja") {
+                makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-d", "Ja");
+                addLineBreak(`subquestion${0}`);
+                makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "ARC-d", "Ja");
+                addLineBreak(`subquestion${1}`);
+                makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "ARC-c", "Ja");
+                addLineBreak(`subquestion${2}`);
+                makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${3}`, 'radio', `subquestion${3}`, "ARC-c", "Ja");
+                addLineBreak(`subquestion${3}`);
+
+            } else {
+                makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "ARC-c", "Ja");
+                addLineBreak(`subquestion${0}`);
+                makeButton(`subquestion${0}`, 'radio', `subquestion${0}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "ARC-c", "Ja");
+                addLineBreak(`subquestion${1}`);
+                makeButton(`subquestion${1}`, 'radio', `subquestion${1}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "ARC-c", "Ja");
+                addLineBreak(`subquestion${2}`);
+                makeButton(`subquestion${2}`, 'radio', `subquestion${2}`, "subquestion", "Nej");
+
+                makeButton(`subquestion${3}`, 'radio', `subquestion${3}`, "ARC-b", "Ja");
+                addLineBreak(`subquestion${3}`);
+            }
+        }
+            break;
     }
 }
 
 function removeSubQuestionPath(node) {
     findAllNextElementSiblings(node).forEach((element) => {
-        
+
         if (element.id.replace(/[0-9]/, '') === "subform") {
             console.log("removeSubQuestionPath");
             console.log(element.id);
@@ -275,17 +297,17 @@ function addFormElement(formID, questionID, questionName, typeOfButton, numOfBut
     const newForm = document.createElement('form');
     newForm.id = formID;
     mainForm.appendChild(newForm);
-    
+
     const form = document.querySelector(`#${formID}`);
     const newElement = document.createElement("fieldset");
     newElement.id = questionID;
     newElement.innerHTML = `<legend> ${questionName} </legend>`;
     form.appendChild(newElement);
-    
+
     for (let i = 0; i < numOfButtons; i++) {
         let textContent = questionContent[`${questionID}`].choices[`choice${i}`].text;
         let value = questionContent[`${questionID}`].choices[`choice${i}`].value;
-        
+
         makeButton(questionID, typeOfButton, questionID, value, textContent);
         addLineBreak(questionID);
     };
@@ -296,14 +318,14 @@ function addSubFormElement(formID, questionID, questionName, typeOfButton, numOf
     const newForm = document.createElement('form');
     newForm.id = formID;
     mainForm.appendChild(newForm);
-    
+
     const form = document.querySelector(`#${formID}`);
     const newElement = document.createElement("fieldset");
     newElement.id = questionID;
     newElement.innerHTML = `<legend> ${questionName} </legend>`;
-    
+
     newElement.style.display = "none";
-    
+
     form.appendChild(newElement);
 };
 
@@ -311,20 +333,20 @@ function addSubFormElement(formID, questionID, questionName, typeOfButton, numOf
 function initPage(quesNum, formNum) {
     let currentQuestion = Object.keys(questionContent)[quesNum];
     let numOfButtons = Object.keys(questionContent[`${currentQuestion}`].choices).length;
-    
+
     addFormElement(`form${formNum}`, `${currentQuestion}`, questionContent[`${currentQuestion}`].ask, "radio", numOfButtons);
 };
 
 /* Add button to question */
 function makeButton(idElement, typeOfButton, name, value, textContent) {
     const element = document.querySelector(`#${idElement}`);
-    
+
     button = document.createElement("input");
     button.type = typeOfButton;
     button.name = name;
     button.value = value;
     element.appendChild(button);
-    
+
     label = document.createElement("label");
     label.textContent = textContent;
     element.appendChild(label);
@@ -340,20 +362,20 @@ function addLineBreak(id) {
 /* Add submit button to the end of page */
 function makeSubmitButton(postToServerFunc, value, nextPage) {
     const element = document.querySelector(`#${MAINFORM}`);
-    
+
     button = document.createElement("input");
     button.type = "submit";
     button.name = "submitBtn";
     button.value = value;
     button.disabled = true;
-    
+
     //button.setAttribute("onclick", `location.href='${nextPage}'`)
 
     element.insertAdjacentElement("afterend", button);
     label = document.createElement("label");
     element.insertAdjacentElement("afterend", label);
 
-  
+
     let sbtn = document.querySelector('[name="submitBtn"]');
     sbtn.addEventListener("click", postToServerFunc);
 }
@@ -362,10 +384,10 @@ function makeSubmitButton(postToServerFunc, value, nextPage) {
    ********************** 
    GRC specific functions
    **********************
-*/ 
+*/
 
 function postFinalGRC() {
-    
+
     calculateGRC(
         parseInt(questionContent[`question${1}`].userInput),
         parseInt(questionContent[`question${2}`].userInput),
@@ -379,7 +401,7 @@ function postFinalGRC() {
     );
 
     //console.log(GRC)
-    //postObjectToServer('GRC', GRC, "http://server.malthelarsen.dk/forms/arc.html")
+    //postObjectToServer('GRC', GRC, `http://${frontendAddr}/form/arc.html`)
 
     let idk = {
         GRC: GRC
@@ -388,15 +410,15 @@ function postFinalGRC() {
     console.log(idk);
 
     postGRCToServer(idk)
-    .then((res) => console.log(res));
+        .then((res) => console.log(res));
 }
 
 function calculateGRC(lenght, los, type) {
     let y = 0;
     if (type > 0) {
-      y = los + type;
+        y = los + type;
     } else {
-      y = 0;
+        y = 0;
     }
     GRC = GRCMatrix[lenght][y];
     return GRC;
@@ -414,48 +436,59 @@ function mitigateGrc(attToGround, parachute, erp, robustness) {
     return GRC;
 }
 
-async function postGRCToServer(grcToPost){
-    //console.log(grcToPost);
-    fetch('http://server.malthelarsen.dk:3000/GRC',
-    {
-    method: 'POST',
-    body: JSON.stringify(grcToPost),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-    })
-    .then(res => {
-    if (res.ok)
-    console.log("Ok");
-    //window.location.href = "http://server.malthelarsen.dk/forms/arc.html";
+async function postGRCToServer(grcToPost) {
+    fetch(`http://${backendAddr}/GRC`,
+        {
+            method: 'POST',
+            body: JSON.stringify(grcToPost),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => {
+            if (res.ok)
+                console.log("Ok");
+            window.location.href = `http://${frontendAddr}/form/arc.html?GRC=${grcToPost.GRC}`;
 
-    console.log(res);
-    })
+            console.log(res);
+        })
     return "All good";
 }
 
 async function postObjectToServer(endpoint, object, redirect = undefined) {
-    console.log(endpoint);
-    fetch(`http://server.malthelarsen.dk:3000/${endpoint}`,
+    return new Promise((resolve, reject) => {
+        fetch(`http://${backendAddr}/${endpoint}`,
         {
             method: 'POST',
             body: JSON.stringify(object),
             headers: { 'Content-Type': 'application/json' }
         })
-        .then(result => result.json())
-        .then(data => {
-            console.log("Ok")
-            if (redirect != undefined)
-            {
-                console.log(redirect)
-                //window.location.href = redirect
+        .then(result => {
+            if (result.ok) {
+                if (redirect != undefined) {
+                    console.log(redirect)
+                    //window.location.href = redirect
+                }
+                resolve();
             }
-            return "GGWP";
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err)
+            reject(err);
+        });
+    })
 }
 
 function postFinalARC() {
+    let objToSend = {
+        arc: ARC,
+        grc: GRC
+    }
+
+    postObjectToServer('ARCGRC', objToSend).then(() => {
+        console.log("GGWP MFER")
+    })
+    .catch(err => console.log("Det ikke for godt det her: \n " + err))
     console.log("POSTING ARC");
 }
 
