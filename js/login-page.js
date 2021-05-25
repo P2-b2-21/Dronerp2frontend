0a2b97c81ee5343f6b1213efcd6b361ab10d7f52
danielalexlarsen <danielalexlarsen@gmail.com>
2021-05-25 09:16:39 +0200

    clean up of code and added comments

--- js/login-page.js
+++ js/login-page.js
@@ -1,8 +1,9 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
-const xhttp = new XMLHttpRequest();
+// Definitions for HTMLElements at the start of the document. Note 'const' is not a constant value, but a constant reference to a certain variable/value  
 
+// EventListener necessary for the function 'login'   
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
 
@@ -15,7 +16,7 @@ loginButton.addEventListener("click", (e) => {
 
 })
 
-//Login Function hihi
+// Login function for login-page, an async function fetching with 'POST' method. if 'userData' is correct = login succesful, otherwise failed. 
 async function login() {
     console.log("Call")
     var userData = {
@@ -41,17 +42,20 @@ async function login() {
     }
 }
 
-var faktaBtn = document.querySelector("#faktaBtn");
+// Function for redirecting user to /faktaomansoegning.html. EventListener on #faktaBtn 
+let faktaBtn = document.querySelector("#faktaBtn");
 faktaBtn.addEventListener("click", function(){
     location.href = "/common/faktaomansoegning.html";
 }) 
 
-var registerBtn = document.querySelector("#registerBtn");
+// Function for redirecting user to /registrering.html. EventListener on #registerBtn
+let registerBtn = document.querySelector("#registerBtn");
 registerBtn.addEventListener("click", function() {
     location.href = "/common/registrering.html"
 })
 
-var headerTitle = document.querySelector("#headerTitle");
+// Function for redirecting user back to front-page "/index.html". EventListener on #headerTitle button 
+let headerTitle = document.querySelector("#headerTitle");
 headerTitle.addEventListener("click", function() {
     location.href = "/index.html";
     console.log(res);
--- js/registrering.js
+++ js/registrering.js
@@ -1,24 +1,25 @@
-
-var loginBtn = document.querySelector("#loginBtn");
+// Function for redirecting user to /login.html. 
+let loginBtn = document.querySelector("#loginBtn");
 loginBtn.addEventListener("click", function(){
     location.href = "/common/login.html";
 })
-    
-var faktaBtn = document.querySelector("#faktaBtn");
+// Function for redirecting user to /faktaomansoegning.html     
+let faktaBtn = document.querySelector("#faktaBtn");
 faktaBtn.addEventListener("click", function(){
     location.href = "/common/faktaomansoegning.html";
 })
-
+// const declaration for HTMLElement registerForm. 
 const registerForm = document.getElementById("register-form");
 
-
+// const declaration for registerBtn (submit button on register page). EventListener on registerBtn, with await 'POST' method for posting user information to backend. 
+// if userData input is correct (response 201), create user and redirect to /login.html. If userData input is incorrect, ask user to re-type. 
 const registerBtn = document.getElementById("register-form-submit");
 registerBtn.addEventListener("click", async (e) => {
     e.preventDefault();
 
     if (registerForm.password.value === registerForm.confirmPassword.value)
     {
-        var userData = {
+        let userData = {
             username: registerForm.username.value,
             password: registerForm.password.value
         }
